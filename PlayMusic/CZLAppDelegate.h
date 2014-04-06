a879523f8ed62f31d991dba8d0bbe9c026b8f79d
czlib <979740349@qq.com>
2014-04-06 22:11:34 +0800

    Initial Commit

---
+++ PlayMusic/CZLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CZLAppDelegate.h
+//  PlayMusic
+//
+//  Created by c on 14-4-6.
+//  Copyright (c) 2014年 czl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
